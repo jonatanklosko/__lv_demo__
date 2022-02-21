c5b728eeb261fcf5a762af862afcf041d81fc13f
Jonatan Kłosko <jonatanklosko@gmail.com>
2022-02-21 16:31:21 +0100

    Demo

--- lib/hibernate_web/live/home_live.ex
+++ lib/hibernate_web/live/home_live.ex
@@ -2,9 +2,22 @@ defmodule HibernateWeb.HomeLive do
   use HibernateWeb, :live_view
 
   @impl true
+  def mount(_params, _session, socket) do
+    {:ok, assign(socket, :counter, 0)}
+  end
+
+  @impl true
   def render(assigns) do
     ~H"""
-    <div></div>
+    <div id="root" phx-hook="Root">
+      <button phx-click="ping">Ping</button>
+    </div>
     """
+  end
+
+  @impl true
+  def handle_event("ping", %{}, socket) do
+    socket = update(socket, :counter, &(&1 + 1))
+    {:noreply, push_event(socket, "ping", %{counter: socket.assigns.counter})}
   end
 end
